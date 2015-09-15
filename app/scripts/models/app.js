b23fd23d7c7d0e24397504f864715709f6d175b6
James Conkling <james.lane.conkling@gmail.com>
2015-09-14 21:08:31 -0700

    add explanatory content

--- app/scripts/models/app.js
+++ app/scripts/models/app.js
@@ -6,7 +6,7 @@ module.exports = Backbone.Model.extend({
   initialize: function() {
     this.pages = new Pages();
     this.set('org', 'Adequate Design Studios');
-    this.set('user', 'Terence Mckenna');
+    this.set('user', 'Horatio');
 
     // fraction of the window size to trigger page breakpoint
       // 0: top; 0.5: middle; 1: bottom
